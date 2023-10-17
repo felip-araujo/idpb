8183690e55615f2fb971a5243338c495c2ba7ee4
felip-araujo <felipearaujoexecutive@gmail.com>
2023-10-17 15:33:03 -0400

    add logica simples com popups

---
+++ main.js
@@ -0,0 +1,5 @@
+
+function abrirPopup(){
+    URL="joinus.html"; 
+    window.open(URL, 'janela', 'width=660, heigth=510, top=100, left=669, scrollbars=yes, status=no, toolbar=no, location=no, directories=no, menubar=no, resizable=no, fullscreen=no');
+}
