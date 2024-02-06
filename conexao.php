7a6d031ed53a0eeb499572675eaec67d639cd977
Christopher Santos <chrisdynamus@gmail.com>
2024-02-06 17:53:52 -0400

    tirando echo desnecessário e linhas extras. agora é o conexao.php certo

--- conexao.php
+++ conexao.php
@@ -9,8 +9,7 @@ try {
     $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
     // Configurar o PDO para lançar exceções em caso de erros
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "";
 } catch (PDOException $e) {
     echo "Falha ao conectar ao banco de dados: " . $e->getMessage();
-} 
+}
 ?>
