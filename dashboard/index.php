137eb3dfb41bd805b1b64d813325261c2aee9f6a
felip-araujo <felipearaujoexecutive@gmail.com>
2024-02-16 08:39:12 -0400

    teste login.css on dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -2,7 +2,8 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="/idpb/login/asstes.login/login.css">
     <title>Dashboard</title>
 </head>
 <body>
