cf8c91d3859a3225f08075b1f3f904179d173615
felip-araujo <felipearaujoexecutive@gmail.com>
2024-02-16 08:52:32 -0400

    4

--- dashboard/index.php
+++ dashboard/index.php
@@ -32,7 +32,7 @@
 
         // Verificar se número da célula foi encontrado
         if ($resultado_celula) {
-            echo "< class='subtitulo p>Nome: " . $resultado_celula['Nome'] . "</p>"; 
+            echo "<class='subtitulo'p>Nome: " . $resultado_celula['Nome'] . "</p>"; 
             echo "<p>Número da Célula: " . $resultado_celula['Celula'] . "</p>"; 
         } else {
             echo "<p>Número da Célula não encontrado.</p>";
