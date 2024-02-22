40ab810dbc56a8f26e3c2bcbd8391a83c317c55b
felip-araujo <felipearaujoexecutive@gmail.com>
2024-02-22 16:48:38 -0400

    enviroment

--- login/redefinir-senha.php
+++ login/redefinir-senha.php
@@ -7,25 +7,15 @@
 
         if($senha1 == $senha2) {
 
-            require 'conexao.php';      
-            $id_user = "SELECT id FROM users2 WHERE email = `$email`"; 
-            // imprimindo a consulta de forma temporaria 
-            $stmt = $pdo->prepare($id_user);
-
-            // Vincular parâmetros
+            require 'conexao.php';     
+            $query = "SELECT id FROM users2 WHERE email = '$email' "; 
+            $stmt = $pdo->prepare($query);
             $stmt->bindParam(':email', $email);
+            $stmt->execute([]);
+            $id = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            print_r($id);
 
-            // Executar a consulta
-            $stmt->execute();
-
-            // Verificar se a consulta retornou algum resultado
-            if ($stmt->rowCount() > 0) {
-                // Recuperar o resultado como uma matriz associativa
-                $row = $stmt->fetch(PDO::FETCH_ASSOC);
-                echo "ID do usuário: " . $row["id"];
-            } else {
-                echo "Nenhum resultado encontrado.";
-            }
+            
             
         } else {
             echo "As senhas não coincidem";
