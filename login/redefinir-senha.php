9565ca5ab0df398e0f7a9dc1c258b1f211ee453e
felip-araujo <felipearaujoexecutive@gmail.com>
2024-02-21 20:47:53 -0400

    teste

--- login/redefinir-senha.php
+++ login/redefinir-senha.php
@@ -1,7 +1,11 @@
-<?php 
-
-echo " teste ";
-echo " aqui vai o formulario para troca de senha através de uma requisição SQL ";
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redefinir Senha</title>
+</head>
+<body>
+    
+</body>
+</html>
