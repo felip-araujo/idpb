f6960f796640e1536a3386ae82446c94ce7a8d17
EdielD7 <129358065+EdielD7@users.noreply.github.com>
2024-02-27 12:47:08 -0400

    fix responsividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
-window.addEventListener("scroll", function () {
-    const header = document.querySelector(".header");
-    const headerHeight = header.offsetHeight;
+document.addEventListener('DOMContentLoaded', function () {
     const viewportHeight = window.innerHeight;
-    const scrollPosition = window.scrollY;
-    if (scrollPosition > viewportHeight) {
-        header.classList.add("header--fixed");
-    } else {
-        header.classList.remove("header--fixed");
-    }
-});
+
+    window.addEventListener("scroll", function () {
+        const scrollPosition = window.scrollY;
+        
+        if (scrollPosition > viewportHeight) {
+            fixHeader();
+        } else {
+            unfixHeader();
+        }
+    });
+})
+
+
+function fixHeader() {
+    const header = document.querySelector(".header");
+    header.classList.add("header--fixed");
+}
+
+function unfixHeader() {
+    const header = document.querySelector(".header");
+    header.classList.remove("header--fixed");
+}
