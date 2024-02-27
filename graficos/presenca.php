f319fd8f6ada6af9eaf279d7823dea5c777c8c7c
Christopher Santos <chrisdynamus@gmail.com>
2024-02-26 20:11:38 -0400

    separei css

--- graficos/presenca.php
+++ graficos/presenca.php
@@ -2,39 +2,7 @@
 <html>
 <head>
     <title>Presença dos Membros da Célula <?php echo isset($_GET['celula']) ? $_GET['celula'] : ''; ?></title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f5f5f5;
-            margin: 0;
-            padding: 0;
-        }
-        h2 {
-            text-align: center;
-            color: #333;
-        }
-        table {
-            border-collapse: collapse;
-            width: 80%;
-            margin: 20px auto;
-            background-color: #fff;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        th, td {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 10px;
-        }
-        th {
-            background-color: #f2f2f2;
-            font-weight: bold;
-        }
-        .date-separator {
-            background-color: #ccc;
-            font-weight: bold;
-            color: #333;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
 
