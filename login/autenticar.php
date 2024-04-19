f0f81292d25829d0ea1b50e660dac626116149b6
felip-araujo <felipearaujoexecutive@gmail.com>
2024-04-19 14:22:43 -0400

    swith

--- login/autenticar.php
+++ login/autenticar.php
@@ -11,23 +11,26 @@
         $busca->execute(); 
         $resultado_usuario =  $busca->fetchAll(PDO::FETCH_ASSOC);
 
-        $id_usuario = $resultado_usuario[0]['id_usuario']; 
+        $id_usuario = $resultado_usuario[0]['id_usuario'];  
+        $nome = $resultado_usuario[0]['nome'];
         // var_dump($id_usuario);
 
         if(password_verify($senha, $resultado_usuario[0]['senha'])){
-            echo ("pasou na verificação"); 
+            
+            $autenticado = true;
+            $_SESSION['autenticado'] = $autenticado;
+        
             $busca_funcao = $pdo->prepare("SELECT * FROM usuarios_funcoes WHERE id_usuario = :id_usuario");
             $busca_funcao->bindParam(':id_usuario', $id_usuario);
             $busca_funcao->execute();
             $resultado_funcao = $busca_funcao->fetchAll(PDO::FETCH_ASSOC);
             $funcao_usuario = $resultado_funcao[0]['id_funcao']; 
             $_SESSION['funcao_usuario'] = $funcao_usuario;
-            // var_dump('A função do usuario é ' . $funcao_usuario); 
 
-
+          
             switch ($funcao_usuario) {
                 case 1:
-                    echo "dashboard do líder {com switch}";
+                    echo "dashboard do líder {com switch}"; 
                     break; 
                 case 2: 
                     echo "dashboard do supervisor {com switch}";
@@ -36,11 +39,11 @@
                     echo "dashboard do coordenador {com switch}";
                     break;
                 case 4:  
-                    echo "dashboard do coordenador {com switch}";
+                    echo "dashboard do coordenador {com switch}"; 
+                    echo '<script>window.location.href="../dashboard/v2/pastor-dashboard.php"</script>'; 
+                    $_SESSION['autenticado'] = true;
                     break;
             }
-            
-
             
         } else {
             echo "<script>alert('Combinação de Email/Senha Incorretos')</script>";
