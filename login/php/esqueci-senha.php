1b533724c78158fd1d69cc6b4f1db9f4173dda4d
felip-araujo <felipearaujoexecutive@gmail.com>
2024-08-21 09:06:44 -0400

    line3

--- login/php/esqueci-senha.php
+++ login/php/esqueci-senha.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception; 
 use PHPMailer\PHPMailer\SMTP;
